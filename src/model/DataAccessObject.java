626fa3366af37481b218462910ba30c5056c01e5
truongnguyenfpthcm2605 <truongnguenlqm@gmail.com>
2022-07-14 15:24:21 +0700

    upload source code new

---
+++ src/model/DataAccessObject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author truong
+ */
+public class DataAccessObject {
+    public List<Object> list = new ArrayList<>();
+
+}
---
+++ src/model/Users.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author truong
+ */
+public class Users {
+    public String user;
+    public String pass;
+    public String role;
+
+    public Users(String user, String pass, String role) {
+        this.user = user;
+        this.pass = pass;
+        this.role = role;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+}
